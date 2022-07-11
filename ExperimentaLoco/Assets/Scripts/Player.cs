21ece963c7555978ad3f31c5e211670b4f254159
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-07-11 19:11:59 +0200

    Fixed code for application of friction / drag to velocity

--- ExperimentaLoco/Assets/Scripts/Player.cs
+++ ExperimentaLoco/Assets/Scripts/Player.cs
@@ -14,8 +14,8 @@ public class Player : MonoBehaviour
 {
     //# Public Variables 
     public float gravity = -9.81f;
-    public float friction = -0.3f;  //< while on ground
-    public float drag = -0.1f;      //< while in air
+    public float friction = 0.3f;  //< while on ground
+    public float drag = 0.1f;      //< while in air
     public int maxExplosivesInWorld;
     public GameObject explosiveSpawnOrigin;
     public GameObject DEBUGExplosiveSpawn;
@@ -51,18 +51,11 @@ public class Player : MonoBehaviour
             velocity.y += gravity * Time.deltaTime;
         if (isGrounded && velocity.y < 0)
             velocity.y = 0;
-        
+
         controller.Move(velocity * Time.deltaTime); //< Multiply by Time.deltaTime a second time, because that's how physics work (https://youtu.be/_QajrabyTJc?t=998)
 
-        // Todo: Depending on if value is below or above 0, go towards 0
-        if (isGrounded)
-        {
-            velocity += new Vector3(friction * Time.deltaTime, 0, friction * Time.deltaTime);
-        }
-        else
-        {
-            velocity += new Vector3(drag * Time.deltaTime, 0, drag * Time.deltaTime);
-        }
+        velocity.x = DecreaseVelocity(velocity.x);
+        velocity.y = DecreaseVelocity(velocity.y);
     }
 
     //# Public Methods 
@@ -72,8 +65,7 @@ public class Player : MonoBehaviour
         //controller.Move(velocity); not needed, unless you want to make a dash, because controller.Move is already being called every frame
     }
 
-    //# Private Methods 
-    private void ThrowExplosive()
+    public void ThrowExplosive()
     {
         Debug.Log($"Player.ThrowExplosive has been called.");
         GameObject newExplosive = Instantiate(explosivePrefab, explosiveSpawnOrigin.transform.position, Quaternion.identity);
@@ -89,7 +81,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void DetonateExplosive()
+    public void DetonateExplosive()
     {
         Debug.Log($"Player.DetonateExplosive has been called.");
         List<GameObject> Reversed_ExplosivesInWorld = ExplosivesInWorld;
@@ -103,5 +95,54 @@ public class Player : MonoBehaviour
         }
     }
 
+    //# Private Methods 
+    private float DecreaseVelocity(float velocity)
+    {
+        if (velocity == 0)     //< Cancel out immediately if input value is 0. (Don't bother running this method at all if player is standing still.)
+            return velocity;
+
+        //> Setting the reductionValue based on if player is grounded or not.
+        float reductionValue;
+        if (isGrounded)
+            reductionValue = friction;
+        else
+            reductionValue = drag;
+
+        //> Applying the reductionValue so that it always diminshes the velocity's value towards 0.
+        if (velocity > 0)
+            velocity -= reductionValue * Time.deltaTime;
+        else if (velocity < 0)
+            velocity += reductionValue * Time.deltaTime;
+
+        return velocity;
+    }
+
     //# Input Event Handlers 
+
+
+    //# ARCHIVED - Nice ideas, but there were better 
+    // private void DecreaseFloat(float value)
+    // {
+    //     if (velocity.x == 0 && velocity.z == 0)  //< Don't bother running this method at all if player is standing still.
+    //         return;
+
+    //     //> Setting the reductionValue based on if player is grounded or in the air.
+    //     float reductionValue;
+    //     if (isGrounded)
+    //         reductionValue = friction;
+    //     else
+    //         reductionValue = drag;
+
+    //     //> Applying the reductionValue so that it always diminshes the velocity towards 0
+    //     //  If velocity in that axis is already 0, then there is no need to edit it.
+    //     if (velocity.x > 0)
+    //         velocity.x -= reductionValue * Time.deltaTime;
+    //     else if (velocity.x < 0)
+    //         velocity.x += reductionValue * Time.deltaTime;
+
+    //     if (velocity.y > 0)                                  //< This is partially redundant code, but I do not know how to improve it currently.
+    //         velocity.y -= reductionValue * Time.deltaTime;
+    //     else if (velocity.y < 0)
+    //         velocity.y += reductionValue * Time.deltaTime;
+    // }
 }
