bbe355b4972780ada94766a3aeccb15c08e8dd48
Neonbackfisch <lili.weirich@stud.h-da.de>
2022-07-13 12:47:53 +0200

    Added soundeffect to landmine
    plays after detonation landmine, also added sound source to _implementet/assets

--- ExperimentaLoco/Assets/Scripts/Explosive.cs
+++ ExperimentaLoco/Assets/Scripts/Explosive.cs
@@ -1,8 +1,8 @@
 //================================================================
 // Darmstadt University of Applied Sciences, Expanded Realities
 // Course:       Travel & Transit in VR (by Philip Hausmeier)
-// Script by:    Daniel Heilmann (771144)
-// Last changed: 12-07-22
+// Script by:    Daniel Heilmann (771144), Lili Weirich (769701)
+// Last changed: 13-07-22
 //================================================================
 
 using System.Collections;
@@ -15,6 +15,7 @@ public class Explosive : MonoBehaviour
     public int explosionRadius = 5;
     public int explosionForce = 5;
     public GameObject radiusIndicator;
+    public AudioSource explosionSound;
 
     //public bool canBeActivated; //< If for some reason, a bomb just cannot / must not be activated.
     //public bool isSelected; //< For a later iteration, where you can detonate bombs you look at.
@@ -32,13 +33,22 @@ public class Explosive : MonoBehaviour
     {
         Debug.Log($"{this.name} has been detonated!");
         Vector3 vectorToPlayer = player.transform.position - this.transform.position;
+        
         if (vectorToPlayer.magnitude <= explosionRadius)
         {
             Debug.Log($"{this.name} has been detonated near player {player.gameObject.name}!");
             player.Launch(vectorToPlayer.normalized * explosionForce);
         }
 
-        Despawn();
+        explosionSound.Play();
+        MeshRenderer[] allMeshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer entry in allMeshRenderers)
+        {
+            entry.enabled = false;
+        }
+
+        StartCoroutine(DestroyAfterSoundDone());
     }
 
     public void Despawn()
@@ -48,5 +58,11 @@ public class Explosive : MonoBehaviour
 
     //# Private Methods 
 
+    IEnumerator DestroyAfterSoundDone()
+    {
+        float clipDuration = explosionSound.clip.length;
+        yield return new WaitForSeconds(clipDuration);
+        Despawn();
+    }
     //# Input Event Handlers 
 }
