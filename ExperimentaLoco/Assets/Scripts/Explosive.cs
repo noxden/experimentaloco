2960567a153647c781ec499e916e686c7e62eb21
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-07-17 21:26:24 +0200

    Hacky implementation of surface normal alignment
    - Removed trigger collider from before, as it was completely unnecessary
    - Rigidbody is now frozen upon collision
    - Reduced size of ExplosivePrefab's collider immensely
    - Refactored Explosive.cs
    - Edited SampleScene to provide needed testing environments

--- ExperimentaLoco/Assets/Scripts/Explosive.cs
+++ ExperimentaLoco/Assets/Scripts/Explosive.cs
@@ -15,29 +15,32 @@ public class Explosive : MonoBehaviour
     public int explosionRadius = 5;
     public int explosionForce = 5;
     public GameObject radiusIndicator;
-    public Collider stickyCollider;
 
     //public bool canBeActivated; //< If for some reason, a bomb just cannot / must not be activated.
     //public bool isSelected; //< For a later iteration, where you can detonate bombs you look at.
 
     //# Private Variables 
-    private AudioSource explosionSound;
+    private AudioSource audioSource;
+    private new Rigidbody rigidbody;
 
     //# Monobehaviour Events 
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Start()
     {
         radiusIndicator.transform.localScale = Vector3.one * 2 * explosionRadius;  //< Needs to be multiplied by 2 for the radius to diameter conversion 
-        explosionSound = GetComponent<AudioSource>();
-
-        stickyCollider.isTrigger = true;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.tag == "Player")
-            return;
-
-        Debug.Log("Collided with something that isn't the player.");
+        AttachToSurface(collision);
     }
 
     //# Public Methods 
@@ -45,17 +48,29 @@ public class Explosive : MonoBehaviour
     {
         Debug.Log($"{this.name} has been detonated!");
 
-        Vector3 playerCenter = new Vector3(player.mainCamera.transform.position.x, player.mainCamera.transform.position.y-1f, player.mainCamera.transform.position.z);
+        Vector3 playerCenter = new Vector3(player.mainCamera.transform.position.x, player.mainCamera.transform.position.y - 1f, player.mainCamera.transform.position.z);
         //< The "Center" / Impact point on the player is always 1 meter below the camera, which should roughly be hip height. Should probably be done via get method in player
         Vector3 vectorToPlayer = playerCenter - this.transform.position;
-        
+
         if (vectorToPlayer.magnitude <= explosionRadius)
         {
             Debug.Log($"{this.name} has been detonated near player {player.gameObject.name}!");
             player.Launch(vectorToPlayer.normalized * explosionForce);
         }
 
-        explosionSound.Play();
+        PlayExplosionSound();
+    }
+
+    public void Despawn()
+    {
+        Destroy(this.gameObject);
+    }
+
+    //# Private Methods 
+
+    private void PlayExplosionSound()
+    {
+        audioSource.Play();
         MeshRenderer[] allMeshRenderers = GetComponentsInChildren<MeshRenderer>();
 
         foreach (MeshRenderer entry in allMeshRenderers)
@@ -66,17 +81,33 @@ public class Explosive : MonoBehaviour
         StartCoroutine(DestroyAfterSoundDone());
     }
 
-    public void Despawn()
-    {
-        Destroy(this.gameObject);
-    }
-
-    //# Private Methods 
     IEnumerator DestroyAfterSoundDone()
     {
-        float clipDuration = explosionSound.clip.length;
+        float clipDuration = audioSource.clip.length;
         yield return new WaitForSeconds(clipDuration);
         Despawn();
+    }
+
+    private void AttachToSurface(Collision collision)
+    {
+        //> Constrain / Freeze rigidbody
+        Debug.Log("Collided with something that isn't the player.");    //< Because it can't collide with the player due to collision layers
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        Debug.DrawRay(transform.position, transform.up, Color.red, 10, false);
+
+        Vector3 collisionNormal = collision.contacts[0].normal;
+        Debug.DrawRay(collision.contacts[0].point, collisionNormal, Color.green, 10, false);
+
+        if (collisionNormal.x != 0)         //< so if (collisionNormal == Vector3.right || collisionNormal == Vector3.left)
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, collisionNormal);
+        else if (collisionNormal.z != 0)    //< so if (collisionNormal == Vector3.forward || collisionNormal == Vector3.back)
+            transform.rotation = Quaternion.LookRotation(Vector3.left, collisionNormal);
+        else if (collisionNormal.y != 0)
+            transform.rotation = Quaternion.LookRotation(Vector3.back, collisionNormal);
+
+        Debug.Log($"Transform Rotation: {transform.rotation}");
+        Debug.Log($"Environment Normal: {collision.contacts[0].normal}");
+        Debug.Log($"Quaternion Euler: {Quaternion.Euler(collisionNormal)}");
     }
 
     //# Input Event Handlers 
