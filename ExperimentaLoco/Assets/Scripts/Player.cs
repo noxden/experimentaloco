24647b7c9f953a64b4d494def67c3630caf8d573
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-07-26 23:54:23 +0200

    Post-Merge Cleanup & minor tweaks

--- ExperimentaLoco/Assets/Scripts/Player.cs
+++ ExperimentaLoco/Assets/Scripts/Player.cs
@@ -8,7 +8,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
