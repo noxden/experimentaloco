719be4b29c7d919a552d666f9045d293d4063357
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-07-26 17:18:31 +0200

    Removed singleton functionality from InputHandler
    - It is completely unnecessary

--- ExperimentaLoco/Assets/Scripts/InputHandler.cs
+++ ExperimentaLoco/Assets/Scripts/InputHandler.cs
@@ -13,24 +13,11 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     //# Public Variables 
-    public static InputHandler Instance { set; get; }
     public Player player;
 
     //# Private Variables 
 
     //# Monobehaviour Events 
-    private void Awake()
-    {
-        if (Instance == null)   //< With this if-structure it is IMPOSSIBLE to create more than one instance.
-        {
-            Instance = this;
-            DontDestroyOnLoad(this.gameObject); //< Referring to the gameObject, this singleton script (class) is attached to.
-        }
-        else
-        {
-            Destroy(this.gameObject);   //< If you somehow still get to create a new singleton gameobject regardless, destroy the new one.
-        }
-    }
 
     private void Start()
     {
